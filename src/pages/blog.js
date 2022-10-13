ed8de1a20bb39712aa895f2eb0b0688e13b9076a
gizem28 <gizemodun28@gmail.com>
2022-10-13 11:27:42 +0200

    mdx content added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,24 +7,29 @@ import Seo from '../components/seo'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="Categories">
-      <ul>
-      {
-        data?.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+     {
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile (filter: {sourceInstanceName: {eq: "blog"}}){
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
